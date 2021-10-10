f0ced597e8e47ef1b58bca7b0cd2053872b52ace
Nibedita Swain <nibeditaswain698@gmail.com>
2021-10-10 16:08:41 +0530

    commit

---
+++ src/app/components/list-student/list-student.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {StudentsService} from '../../students.service';
+@Component({
+  selector: 'app-list-student',
+  templateUrl: './list-student.component.html',
+  styleUrls: ['./list-student.component.css']
+})
+export class ListStudentComponent implements OnInit {
+
+  constructor(private student:StudentsService) { }
+  studentData:any=[];
+  dtOptions:DataTables.Settings={};
+  ngOnInit(): void {
+    this.dtOptions={
+      pagingType:'full_numbers',
+      pageLength:5,
+      processing:true,
+      lengthMenu:[5,10,15,50]
+    }
+    this.student.getAllStudent().subscribe((allData)=>{
+      console.log(allData);
+      this.studentData=allData;
+    });
+  }
+
+deleteStudent(student_id:any){
+ // console.log(student_id);
+  this.student.deleteStudent(student_id).subscribe((result)=>{
+    //console.log(result);
+    this.ngOnInit();
+  });
+}
+}
+
